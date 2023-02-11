d916bf5c078c346bbf2e77ccee0575036c87b541
Potados <potados@inu.ac.kr>
2023-02-12 02:27:21 +0900

    chore: 인편 주소 탑재

--- src/application/stories/HelpThisPoorGuy.tsx
+++ src/application/stories/HelpThisPoorGuy.tsx
@@ -5,10 +5,10 @@ export default function HelpThisPoorGuy() {
   return (
     <Story
       title="도와주세요"
-      body="가엾은 병준이를 위해 편지 한 장을 기부해주세요. 당신의 한 마디가 병준이를 살립니다."
+      body="가엾은 병준이를 위해 편지 한 장을 기부해주세요. 2월 27일 낮 9시부터 보낼 수 있어요."
       image="/images/light.png"
       actionLabel="편지 쓰러 가기"
-      actionHref="mailto:potados99@gmail.com"
+      actionHref="https://atc.airforce.mil.kr:444/user/emailPicViewSameMembers.action?siteId=last2&searchName=송병준&searchBirth=19990211"
     />
   );
 }
