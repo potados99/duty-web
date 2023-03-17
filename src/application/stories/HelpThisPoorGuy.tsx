b574e79e848d1eec3cb278e426ab7a132c00abfa
Potados <potados@inu.ac.kr>
2023-03-17 16:40:57 +0900

    feat: 특기학교 편지 링크로 교체

--- src/application/stories/HelpThisPoorGuy.tsx
+++ src/application/stories/HelpThisPoorGuy.tsx
@@ -5,10 +5,10 @@ export default function HelpThisPoorGuy() {
   return (
     <Story
       title="도와주세요"
-      body="가엾은 병준이를 위해 편지 한 장을 기부해주세요. 2월 27일 낮 9시부터 보낼 수 있어요."
+      body="겨우 훈련단을 탈출한 병준이는 다시 특기학교로 붙잡혀 들어갔습니다. 가엾은 병준이를 위해 편지 한 장을 기부해주세요. 3월 20일 낮 10시부터 보낼 수 있어요."
       image="/images/light.png"
       actionLabel="편지 쓰러 가기"
-      actionHref="https://www.airforce.mil.kr/user/indexSub.action?codyMenuSeq=156893223&siteId=last2&menuUIType=sub&dum=dum&command2=getEmailList& searchName=송병준&searchBirth=19990211&memberSeq=327492896"
+      actionHref="https://atc.airforce.mil.kr:444/user/indexSub.action?codyMenuSeq=156894686&siteId=tong-new&menuUIType=top&dum=dum&command2=getEmailList&searchName=송병준&searchBirth=19990211&memberSeq=329123001"
       
     />
   );
