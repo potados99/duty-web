14897efefc66b0b42de740bbde661bf7bb7b41a9
potados99 <potados99@gmail.com>
2023-04-22 11:19:06 +0900

    chore: 커버 색상 변경

--- src/application/cover/Cover.tsx
+++ src/application/cover/Cover.tsx
@@ -6,24 +6,30 @@ import FadeIn from "react-fade-in";
 export default function Cover() {
   return (
     <Wrapper onClick={() => window.scrollTo(0, window.innerHeight)}>
-      <FadeIn delay={300} transitionDuration={600}>
-        <BigText
-          style={{
-            color: "#234",
-            marginTop: 180,
-            marginLeft: 30,
-            marginRight: 30,
-            textAlign: "center",
-          }}
-        >
-          병준이의
-          <br />
-          군대 여행
-        </BigText>
-      </FadeIn>
-      <FadeIn delay={1000} transitionDuration={1000}>
-        <img src="/arrow.svg" width="50" style={{ marginBottom: 48 }} />
-      </FadeIn>
+      <TitleBox>
+        <FadeIn delay={300} transitionDuration={600}>
+          <BigText
+            style={{
+              color: "#234",
+              textAlign: "center",
+            }}
+          >
+            병준이의
+            <br />
+            군대 여행
+          </BigText>
+        </FadeIn>
+      </TitleBox>
+      <ArrowBox>
+        <FadeIn delay={1000} transitionDuration={1000}>
+          <img
+            src="/arrow.svg"
+            width="50"
+            style={{ marginBottom: 48 }}
+            alt="arrow"
+          />
+        </FadeIn>
+      </ArrowBox>
     </Wrapper>
   );
 }
@@ -37,6 +43,15 @@ const Wrapper = styled.div`
 
   height: 100vh;
 
-  background: linear-gradient(white, #f2f5ff, #dadefa);
+  background: linear-gradient(white, #f9f2ff, #ecdafa);
   word-break: keep-all;
 `;
+
+const TitleBox = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ArrowBox = styled.div``;
